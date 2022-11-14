9ed266dbb28d1fc801a5cbce045c94ed1d98a37f
Liam <55931056+latitudinallight@users.noreply.github.com>
2022-11-14 11:03:32 +0000

    Functioning Update to rfcomb
    Code looked over and now functioning, next steps are the following:
    
    1. Improve file input to handle both files at once (can then also be recycled in future scripts)
    2. Regex, perhaps also an options config - so that it's not necessary to have exact sequence input (though I'd still recommend that)

--- revforcombinepeptide.py
+++ revforcombinepeptide.py
@@ -1,96 +1,78 @@
-# code is still a work in progress, will be released publicly once completed and tested on both a test case file and then an instance of actual data
-
-import os
 import re
+import os
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
+import filedialogue as fd
 
-def string_splitter(file1, file2):
+def splitCombiner(file1, file2):
+
+    # Declarations, mit is an empty list meaning "merge into this"
+    mit = []
+    temp = ["","",""]
+    seq_found = 0
+    
+    # Open file1.
     with open(file1, 'r') as infile:
-        input_cwd = os.path.dirname(file1)
+    
+        # This generates a list of lines from the file.
         ori_list1 = infile.readlines()
-        for line in ori_list1: # now for writing the actual file
-            line = line.replace('  ', '    ') # evens out the spacing for splitting purposes
-            line = re.split('    ', line) # splits line
-            init_seq_1 = line[3] # pick line 3, this should be the DNA sequence, because for whatever reason DNA is that line
-            init_peptide_1 = line[0]
-    with open(file2, 'r') as infile2:
-        input_cwd = os.path.dirname(file2)
-        ori_list2 = infile2.readlines()
-        for line2 in ori_list2: # now for writing the actual file
-            line2 = line.replace('  ', '    ') # evens out the spacing for splitting purposes
-            line2 = re.split('    ', line) # splits line
-            init_seq_2 = line2[3] # pick line 3, this should be the DNA sequence, because for whatever reason DNA is that line
-            init_peptide_2 = line2[0]
-    with open(input_cwd + '\\' + 'RFComb_' + (os.path.basename(file1)), 'w') as outfile:
-        splitCombiner(init_seq_1, init_peptide_1, init_seq_2, init_peptide_2)
-
-
-def splitCombiner(init_seq_1, init_peptide_1, init_seq_2, init_peptide_2):
-    for string in ori_list2:
-        if(re.match(init_peptide_1, string)):
-            string_match = string
-            string_match = string_match.replace('  ', '    ') # evens out the spacing for splitting purposes
-            string_match = re.split('    ', string_match) # splits line
-            lineval_comb = line[2] + string_match[2]
-            
-            finalSeq_comp = ''.join([protein, "    ", line[2], "  ", forSeq, "\n"]) # put it all together, that's it
-            outfile.write(finalSeq_comp) # write it
-        else:
         
-# create the root window
+        # Now we go through the list item by item, or line by line.
+        for line in ori_list1:
+        
+            # re.split returns a list. In this step you overwrite your individual line (an element in a list) with a list of four splits.
+            # CIPMSQRQC    221  TGTATTCCTATGTCGCAGCGTCAGTGT is a sample line. It is split into:
+            # ["CIPMSQRQC","221","TGTATTCCTATGTCGCAGCGTCAGTGT"]
+            line = re.split('\s+', line)
+            
+            # Next I’ll add the first to third result of the split to the master list. 
+            
+            temp[1] = line[0]
+            temp[0] = int(line[1])
+            temp[2] = line[2].strip()
+            
+            mit.append(temp.copy())       
+    
+    print(mit)
+    
+    # file2 should be eerily similar.
+    with open(file2, 'r') as infile2:
+    
+        # Generate list of lines. Iterate.
+        ori_list2 = infile2.readlines()
+        for string in ori_list2:
+        
+            # Split as before.
+            string = re.split('\s+', string)
+            
+            # Now we check if our line is already in the master list by checking if the sequence is already in it.                   
+            
+            seq_found = 0
+            
+            for item in mit:
+                if item[2] == string[2].strip():
+                    item[0] += int(string[1])
+                    seq_found = 1
+
+            # and if it’s not, add the line to the master list.
+            if seq_found == 0:
+                temp[1] = string[0]
+                temp[0] = int(string[1])
+                temp[2] = string[2].strip()
+                
+                mit.append(temp.copy())
+                
+    # Output.
+    # Get directory, start writing outfile.
+    input_cwd = os.path.dirname(file1)
+    with open(input_cwd + '\\' + 'RFComb_' + (os.path.basename(file1)), 'w') as outfile:
+        
+        mit.sort(reverse=True) # Sorting in descending order
+        for item in mit:
+            outfile.write(item[1] + "\t" + str(item[0]) + "\t" + item[2] + "\n") # Write file
+
+# tkinter, ask for file input
 root = tk.Tk()
-root.title('Tkinter File Dialog')
-root.resizable(False, False)
-root.geometry('300x150')
-
-def select_files():
-    filetypes = (
-        ('text files', '*.txt'),
-        ('All files', '*.*')
-    )
-    file1 = fd.askopenfilenames(
-        title='Load files',
-        initialdir='/',
-        filetypes=filetypes)
-    showinfo(
-        title='Selected Files',
-        message=file1
-    )
-def select_files2():
-    filetypes = (
-        ('text files', '*.txt'),
-        ('All files', '*.*')
-    )
-    file2 = fd.askopenfilenames(
-        title='Load files',
-        initialdir='/',
-        filetypes=filetypes)
-    showinfo(
-        title='Selected Files',
-        message=file2
-    )
-# Load First File button
-open_button = ttk.Button(
-    root,
-    text='Load First File',
-    command=select_files
-)
-# Load Second File button
-open_button = ttk.Button(
-    root,
-    text='Load Second File',
-    command=select_files2
-)
-
-# start process button
-combine_button = ttk.Button(
-    root,
-    text = 'Combine Files',
-    command = string_splitter)
-
-open_button.pack(expand=True)
-
-root.mainloop()
+root.withdraw()
+file1 = fd.askopenfilename(parent=root, title='Choose a file')
+file2 = fd.askopenfilename(parent=root, title='Choose a file')
+splitCombiner(file1, file2)
