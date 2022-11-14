478d4c825b7202d7c3bbbf44c8260024cb1eea8a
Liam <55931056+latitudinallight@users.noreply.github.com>
2022-11-14 14:00:24 +0000

    File Input Update
    Overhauled file input to be more simplified and to work for more than two files (this is for other, future scripts, please do not use anything other than the expected two files unless you want results that aren't representative).
    
    Next will be to include more functional and effective regex.

--- revforcombinepeptide.py
+++ revforcombinepeptide.py
@@ -1,78 +1,54 @@
 import re
 import os
 import tkinter as tk
-import filedialogue as fd
+import tkinter.filedialog as fd
 
-def splitCombiner(file1, file2):
-
-    # Declarations, mit is an empty list meaning "merge into this"
-    mit = []
-    temp = ["","",""]
-    seq_found = 0
-    
-    # Open file1.
-    with open(file1, 'r') as infile:
-    
-        # This generates a list of lines from the file.
-        ori_list1 = infile.readlines()
-        
-        # Now we go through the list item by item, or line by line.
-        for line in ori_list1:
-        
-            # re.split returns a list. In this step you overwrite your individual line (an element in a list) with a list of four splits.
-            # CIPMSQRQC    221  TGTATTCCTATGTCGCAGCGTCAGTGT is a sample line. It is split into:
-            # ["CIPMSQRQC","221","TGTATTCCTATGTCGCAGCGTCAGTGT"]
-            line = re.split('\s+', line)
-            
-            # Next I’ll add the first to third result of the split to the master list. 
-            
-            temp[1] = line[0]
-            temp[0] = int(line[1])
-            temp[2] = line[2].strip()
-            
-            mit.append(temp.copy())       
-    
-    print(mit)
-    
-    # file2 should be eerily similar.
-    with open(file2, 'r') as infile2:
+def mergeFile(file, mit, temp):
+    with open(file, 'r') as infile:
     
         # Generate list of lines. Iterate.
-        ori_list2 = infile2.readlines()
-        for string in ori_list2:
+        list_lines = infile.readlines()
+        for line in list_lines:
         
-            # Split as before.
-            string = re.split('\s+', string)
+            # Split line.
+            line = re.split('\s+', line)
             
-            # Now we check if our line is already in the master list by checking if the sequence is already in it.                   
+            # Check if our line is already in the master list by checking if the sequence is already in it.                   
             
             seq_found = 0
             
             for item in mit:
-                if item[2] == string[2].strip():
-                    item[0] += int(string[1])
+                if item[2] == line[2].strip():
+                    item[0] += int(line[1])
                     seq_found = 1
 
-            # and if it’s not, add the line to the master list.
+            # If not, add the line to the master list.
             if seq_found == 0:
-                temp[1] = string[0]
-                temp[0] = int(string[1])
-                temp[2] = string[2].strip()
+                temp[1] = line[0]
+                temp[0] = int(line[1])
+                temp[2] = line[2].strip()
                 
                 mit.append(temp.copy())
                 
-    # Output.
-    # Get directory, start writing outfile.
-    input_cwd = os.path.dirname(file1)
-    with open(input_cwd + '\\' + 'RFComb_' + (os.path.basename(file1)), 'w') as outfile:
-        
-        mit.sort(reverse=True) # Sorting in descending order
-        for item in mit:
-            outfile.write(item[1] + "\t" + str(item[0]) + "\t" + item[2] + "\n") # Write file
 
-# tkinter, ask for file input
+def splitCombiner(files):
+
+    mit = []
+    temp = ["","",""]
+    
+    for file in files:
+        mergeFile(file, mit, temp) # Call for file merger seen above.
+                
+    # Get directory, start writing outfile.
+    input_cwd = os.path.dirname(files[0])
+    with open(input_cwd + '\\' + 'RFComb_' + (os.path.basename(files[0])), 'w') as outfile:
+        
+        mit.sort(reverse=True) # Sort in descending order.
+        for item in mit:
+            outfile.write(item[1] + "\t" + str(item[0]) + "\t" + item[2] + "\n")
+
+# Tkinter options and initial file request
 root = tk.Tk()
 root.withdraw()
-file1 = fd.askopenfilename(parent=root, title='Choose a file')
-file2 = fd.askopenfilename(parent=root, title='Choose a file')
-splitCombiner(file1, file2)
+files = fd.askopenfilenames(parent=root, title='Choose files to merge')
+splitCombiner(files)
